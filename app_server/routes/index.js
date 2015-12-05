0b4feb241e076d5d92c69333a4a711cd23997184
Michael Wiss <michael.wiss@gmail.com>
2015-12-05 10:21:40 -0600

    added bootstrap, added amelia theme

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var homepageController = function (req, res) {
-	res.render('index', { title: 'Express' });
-};
+var ctrlMain = require('../controllers/main');
+
 
 /* GET home page. */
-router.get('/', homepageController);
+router.get('/', ctrlMain.index);
 
 
 
