7da666a2da4fb0e67433e3b51c782d988cfae83b
Michael Wiss <michael.wiss@gmail.com>
2015-12-09 11:13:09 -0600

    complete connection file

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -12,3 +12,30 @@ mongoose.connection.on('error',function (err) {
 mongoose.connection.on('disconnected', function () {
   console.log('Mongoose disconnected');
 });
+
+var gracefulShutdown = function (msg, callback) {
+	mongoose.connection.close(function () {
+		console.log('Mongoose disconnected through ' + msg);
+		calback();
+	});
+};
+
+//For nodemon restarts
+process.once('SIGUSR2', function () {
+   gracefulShutdown('nodemon restart', function () {
+   	  process.kill(process.pid, 'SIGUSR2');
+   });
+});
+//For app termonation
+process.on('SIGINT', function() {
+	gracefulShutdown('app termination', function () {
+		process.exit(0);
+	});
+});
+// For Heroku app termination
+process.on('SIGTERM', function() {
+	gracefulShutdown('Heroku app shutdown', function () {
+		process.exit(0);
+	});
+});
+
