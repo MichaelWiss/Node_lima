35b8ddcc54fe6ee1c7f8a2e75a8fb2d56312785c
Michael Wiss <michael.wiss@gmail.com>
2015-12-13 16:01:01 -0600

    error catching added to readone controller

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -17,11 +17,26 @@ module.exports.locationsCreate = function (req, res) {
 
 /* Get location */
 module.exports.locationsReadOne = function (req, res) {
+  if (req.params && req.params.locationid) {
    Loc
     .findById(req.params.locationid)
     .exec(function(err, location) {
-      sendJsonResponse(res, 200, location);
+     if (!location) {
+       sendJsonResponse (res, 404, {
+       	  "message": "locationid not found"
+       });
+       return
+     } else if (err) {
+       sendJsonResponse(res, 200, location);
+       return;
+     }
+     sendJsonResponse(res, 200, location);
     });
+   } else {
+   	 sendJsonResponse (res, 404, {
+   	 	"message": "No locationid in request"
+   	 });
+   }
 };
 
 /* put location */
