b1c24c2c7d97ce2b88b1dcfaa315b2212e0dcbb4
Michael Wiss <michael.wiss@gmail.com>
2016-03-05 16:14:42 -0600

    added renderHomepage to homelist controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -32,33 +32,24 @@ var renderHomepage = function(req, res, responseBody){
 
 /* Get 'home' page */
 module.exports.homelist = function(req, res){
-	res.render('locations-list', { 
-		title: 'Lo8r - find a place to work with wifi',
-		pageHeader: {
-		  title: "Loc8r",
-		  strapline: "Find places to work with wifi near you!"
-	    },
-	    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-	    locations: [{
-	   	  name: 'Starcups',
-	   	  address: '125 High Street, Reading, RG6 1ps',
-	   	  rating: 3,
-	   	  facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-	   	  distance: '100m'
-	   	},{
-	   	  name: 'Cafe Hero',
-	   	  address: '125 High Street, Reading, RG6 1PS',
-	   	  rating: 4,
-	   	  facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-	   	  distance: '200m'
-	   	},{
-	   	  name: 'Burger Queen',
-	   	  address: '125 High Street, Reading, RG6 1PS',
-	   	  rating: 2,
-	   	  facilities: ['Food', 'Premium wifi'],
-	   	  distance: '250m'
-	   }]
-	});
+   var requestOptions, path;
+   path = '/api/locations';
+   requestOptions = {
+   	  url : apiOptions.server + path,
+   	  method : "Get",
+   	  json : {},
+   	  qs : {
+   	  	lng : -0.7992599,
+   	  	lat : 51.378091,
+   	  	maxDistance : 20
+   	  }
+   };
+   request(
+     requestOptions,
+     function(err, response, body) {
+     	renderHomepage(req, res);
+     }
+   	);
 };
 
 /* Get 'Location info' page */
