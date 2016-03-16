423d41b4d2167e1b708418d1132c7afd7fb19d9a
Michael Wiss <michael.wiss@gmail.com>
2016-03-16 15:07:14 -0500

    bug

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -135,7 +135,7 @@ module.exports.locationInfo = function(req, res){
 
 var renderReviewForm = function (req, res, locDetail) {
   res.render('location-review-form', {
-  	title: 'Review ' + locDetail.name + locDetail.name },
+  	title: 'Review ' + locDetail.name + ' on Loc8r',
   	pageHeader: { title: 'Review ' + locDetail.name },
   	error: req.query.err
   });
