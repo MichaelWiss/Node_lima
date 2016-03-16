9cc7ab39181317ed7e6f1fa1aec5278448e079ac
Michael Wiss <michael.wiss@gmail.com>
2016-03-16 17:58:00 -0500

    bugs

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,4 +1,4 @@
-var mongoose = require( 'mongoose' );
+var mongoose = require('mongoose');
 var gracefulShutdown;
 var dbURI = 'mongodb://localhost/Loc8r';
 if (process.env.NODE_ENV === 'production') {
--- app_api/models/locations.js
+++ app_api/models/locations.js
@@ -1,4 +1,4 @@
-var mongoose = require( 'mongoose' );
+var mongoose = require('mongoose');
 
 var reviewSchema = new mongoose.Schema({
 	author: String,
