6097893faab8f609a898cf2c23ee960c4f278666
Michael Wiss <michael.wiss@gmail.com>
2016-03-22 23:17:11 -0500

    location list bug

--- public/angular/loc8rApp.js
+++ public/angular/loc8rApp.js
@@ -16,8 +16,8 @@ var locationListCtrl = function ($scope) {
 			facilities: ['Hot drinks', 'Food', 'Premium wifi', 'Alcoholic drinks'],
 			distance: '0.7865456',
 			_id: '5370a35f2536f6785f8dfb6a'
-        }]};
-};
+       }]};
+    };
 
 angular
    .module('loc8rApp')
