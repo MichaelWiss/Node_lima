fc87521d197831075eeb42b969b3e2b615d4bd63
Michael Wiss <michael.wiss@gmail.com>
2016-03-24 23:04:09 -0500

    bug

--- app_client/app.js
+++ app_client/app.js
@@ -1,12 +1,16 @@
-angular.module('loc8rApp', ['ngRoute'] []);
+angular.module('loc8rApp', ['ngRoute']);
 
-function config ($routeProvider) {
-	$routeProvider
-	  .when('/', {
-	  })
-	  .otherwise({redirectTo: '/'});
-}
+  function config ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'home/home.view.html',
+        controller: 'homeCtrl',
+        controllerAs: 'vm'
+      })
+      .otherwise({redirectTo: '/'});
+  }
 
-angular
-  .module('loc8rApp')
-  .config(['$routeProvider', config]);
+  angular
+    .module('loc8rApp')
+    .config(['$routeProvider', config]);
+
