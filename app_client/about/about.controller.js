b96005a5835a5e44c98f4586bef1477b1194f645
Michael Wiss <michael.wiss@gmail.com>
2016-03-29 21:58:10 -0500

    about bug

--- app_client/about/about.controller.js
+++ app_client/about/about.controller.js
@@ -10,7 +10,7 @@
 		vm.pageHeader = {
 			title: "About Loc8r",
 		};
-		vm.man = {
+		vm.main = {
 			content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit.'
         }; 
     }
