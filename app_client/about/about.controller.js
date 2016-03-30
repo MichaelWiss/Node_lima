36842fc203c72adb2d699e7464b47686ff1f0eb6
Michael Wiss <michael.wiss@gmail.com>
2016-03-30 10:20:03 -0500

    used html code for apostrophe

--- app_client/about/about.controller.js
+++ app_client/about/about.controller.js
@@ -12,7 +12,7 @@
 		};
 		vm.main = {
 			content: 'Loc8r was created to help people find places to sit down
-			 and get a bit of work done.\n\nYOLO migas organic etsy, leggings four loko keffiyeh viral butcher trust fund XOXO pitchfork intelligentsia fashion axe. Occupy offal raw denim listicle dreamcatcher, kombucha readymade bitters microdosing. Chia typewriter forage, freegan heirloom bushwick keffiyeh everyday carry +1 sustainable you probably haven\nt heard of them artisan. Locavore VHS literally chillwave, flexitarian kale chips gastropub brooklyn migas. Gentrify tousled four dollar toast freegan normcore franzen, irony PBR&B crucifix offal keytar viral. Sriracha tilde meh, vinyl selfies pitchfork chambray photo booth. Brunch scenester synth poutine, knausgaard green juice fashion axe organic portland cold-pressed sartorial food truck vice blue bottle.'
+			 and get a bit of work done.\n\nYOLO migas organic etsy, leggings four loko keffiyeh viral butcher trust fund XOXO pitchfork intelligentsia fashion axe. Occupy offal raw denim listicle dreamcatcher, kombucha readymade bitters microdosing. Chia typewriter forage, freegan heirloom bushwick keffiyeh everyday carry +1 sustainable you probably haven&#39;t heard of them artisan. Locavore VHS literally chillwave, flexitarian kale chips gastropub brooklyn migas. Gentrify tousled four dollar toast freegan normcore franzen, irony PBR&B crucifix offal keytar viral. Sriracha tilde meh, vinyl selfies pitchfork chambray photo booth. Brunch scenester synth poutine, knausgaard green juice fashion axe organic portland cold-pressed sartorial food truck vice blue bottle.'
         }; 
     }
 
