64cb33be37d439cbeb167d50f683c3a3d4b0d4c3
Michael Wiss <michael.wiss@gmail.com>
2016-03-31 14:18:25 -0500

    pass loc8rData object to review controller

--- app_client/reviewModal/reviewModal.controller.js
+++ app_client/reviewModal/reviewModal.controller.js
@@ -4,14 +4,28 @@
 	  .module('loc8rApp')
 	  .controller('reviewModalCtrl', reviewModalCtrl);
 
-	reviewModalCtrl.$inject = ['$modalInstance'];
-	function reviewModalCtrl ($modalInstance) {
+	reviewModalCtrl.$inject = ['$modalInstance', 'loc8rData', 'locationData'];
+	function reviewModalCtrl ($modalInstance, loc8rData, locationData) {
 		var vm = this;
+		vm.locationData = locationData;
+
+		vm.onSubmit = function () {
+			vm.formError = "";
+			if(!vm.formData.name || !vm.formData.rating || !vm.formData.reviewText) {
+			   vm.form.Error = "All fields required, please try again";
+			   return false;
+			} else {
+			console.log(vm.formData);
+			return false;
+		} else {
+			vm.doAddReview(vm.locationData.locationid, vm.formData);
+		 }
+		};
         
         var.modal = {
         	cancel : function () {
         		$modalInstance.dismiss('cancel');
         	}
-        };
-	}
+          };
+	    }
 })();
