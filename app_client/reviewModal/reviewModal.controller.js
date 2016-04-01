413d02f4f53de25e3705a4b3d4875317f069b45d
Michael Wiss <michael.wiss@gmail.com>
2016-03-31 21:39:27 -0500

    bug

--- app_client/reviewModal/reviewModal.controller.js
+++ app_client/reviewModal/reviewModal.controller.js
@@ -1,46 +1,47 @@
 (function () {
 
-	angular
-	  .module('loc8rApp')
-	  .controller('reviewModalCtrl', reviewModalCtrl);
+  angular
+    .module('loc8rApp')
+    .controller('reviewModalCtrl', reviewModalCtrl);
 
-	reviewModalCtrl.$inject = ['$modalInstance', 'loc8rData', 'locationData'];
-	function reviewModalCtrl ($modalInstance, loc8rData, locationData) {
-		var vm = this;
-		vm.locationData = locationData;
+  reviewModalCtrl.$inject = ['$modalInstance', 'loc8rData', 'locationData'];
+  function reviewModalCtrl ($modalInstance, loc8rData, locationData) {
+    var vm = this;
+    vm.locationData = locationData;
 
-		vm.onSubmit = function () {
-			vm.formError = "";
-			if(!vm.formData.name || !vm.formData.rating || !vm.formData.reviewText) {
-			   vm.form.Error = "All fields required, please try again";
-			   return false;
-			} else {
-			console.log(vm.formData);
-			return false;
-		} else {
-			vm.doAddReview(vm.locationData.locationid, vm.formData);
-		 }
-		};
+    vm.onSubmit = function () {
+      vm.formError = "";
+      if (!vm.formData.name || !vm.formData.rating || !vm.formData.reviewText) {
+        vm.formError = "All fields required, please try again";
+        return false;
+      } else {
+        vm.doAddReview(vm.locationData.locationid, vm.formData);
+      }
+    };
 
-		vm.doAddReview = function (locationid, formData) {
-			loc8rData.addReviewById(locationid, {
-				author : formData.name,
-				rating : formData.rating,
-				reviewText : formData.reviewText
-			})
-			.success(function (data) {
-				console.log("Success!");
-			})
-			.error(function (data) {
-				vm.formError = "Yur review has not been saved, try again";
-			});
-			return false;
-		};
-        
-        vm.modal = {
-        	cancel : function () {
-        		$modalInstance.dismiss('cancel');
-        	}
-          };
-	    }
+    vm.doAddReview = function (locationid, formData) {
+      loc8rData.addReviewById(locationid, {
+        author : formData.name,
+        rating : formData.rating,
+        reviewText : formData.reviewText
+      })
+        .success(function (data) {
+          vm.modal.close(data);
+        })
+        .error(function (data) {
+          vm.formError = "Your review has not been saved, please try again";
+        });
+      return false;
+    };
+
+    vm.modal = {
+      close : function (result) {
+        $modalInstance.close(result);
+      },
+      cancel : function () {
+        $modalInstance.dismiss('cancel');
+      }
+    };
+  }
+
 })();
