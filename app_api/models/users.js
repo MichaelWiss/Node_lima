7b658c2cf5e13b12bff77a2787a1257642f82247
Michael Wiss <michael.wiss@gmail.com>
2016-04-02 13:30:45 -0500

    added setPassword method with salt and hash

--- app_api/models/users.js
+++ app_api/models/users.js
@@ -1,4 +1,5 @@
 var mongoose = require( 'mongoose' );
+var crypto = require('crypto');
 
 var userSchema = new mongoose.Schema({
 	email: {
@@ -12,4 +13,9 @@ var userSchema = new mongoose.Schema({
 	},
 	hash: String,
 	salt: String
-    });
+});
+
+userSchema.methods.setPassword = function(password) {
+	this.salt = crypto.randomBytes(16).toString('hex');
+	this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
