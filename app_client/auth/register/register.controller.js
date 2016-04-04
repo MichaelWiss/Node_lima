c052b11cbeb214d33796e35e6c0e737a2ec3e4a4
Michael Wiss <michael.wiss@gmail.com>
2016-04-03 21:40:40 -0500

    error capturing register.controller.js

--- app_client/auth/register/register.controller.js
+++ app_client/auth/register/register.controller.js
@@ -21,6 +21,19 @@
 		vm.returnPage = $location.search().page || '/';
 
 		vm.onSubmit = function () {
+		  vm.formError = "";
+		  if (!vm.credentials.name || !vm.credentials.email || !vm.credentials.password) {
+		  	vm.formError = "All fields required, please try again";
+		  	return false;
+		  } else {
+		  	vm.doRegister();
+		  }
        };
+
+       vm.doRegister = function() {
+       	vm.forError = "";
+       	authentication
+       	   .
+       }
 	}
 })();
