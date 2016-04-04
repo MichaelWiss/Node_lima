c110c49a49c977f25e842479eb5334727047de3d
Michael Wiss <michael.wiss@gmail.com>
2016-04-04 13:20:21 -0500

    added login.controller.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ var appClientFiles = [
   'app_client/app.js',
   'app_client/home/home.controller.js',
   'app_client/about/about.controller.js',
+  'app_client/auth/login/login.controller.js',
   'app_client/auth/register/register.controller.js',
   'app_client/reviewModal/reviewModal.controller.js',
   'app_client/common/services/geolocation.service.js',
