678b8f648d959cc6d62cca045657eb7f510858d8
Michael Wiss <michael.wiss@gmail.com>
2016-04-08 16:12:11 -0500

    review post bug

--- app_client/reviewModal/reviewModal.controller.js
+++ app_client/reviewModal/reviewModal.controller.js
@@ -9,19 +9,10 @@
     var vm = this;
     vm.locationData = locationData;
 
-    vm.onSubmit = function () {
-      vm.formError = "";
-      if (!vm.formData.name || !vm.formData.rating || !vm.formData.reviewText) {
-        vm.formError = "All fields required, please try again";
-        return false;
-      } else {
-        vm.doAddReview(vm.locationData.locationid, vm.formData);
-      }
-    };
+    
 
     vm.doAddReview = function (locationid, formData) {
       loc8rData.addReviewById(locationid, {
-        author : formData.name,
         rating : formData.rating,
         reviewText : formData.reviewText
       })
@@ -32,6 +23,16 @@
           vm.formError = "Your review has not been saved, please try again";
         });
       return false;
+    };
+
+    vm.onSubmit = function () {
+      vm.formError = "";
+      if (!vm.formData.rating || !vm.formData.reviewText) {
+        vm.formError = "All fields required, please try again";
+        return false;
+      } else {
+        vm.doAddReview(vm.locationData.locationid, vm.formData);
+      }
     };
 
     vm.modal = {
